21de7ff113b9b9637cffc29fe2040031225efb96
youhei usami <usao9110@icloud.com>
2025-07-26 14:28:26 +0400

    7/26

---
+++ src/composables/useWord.ts
@@ -0,0 +1,11 @@
+import { Word } from "../types/Word";
+
+
+export const useWords = () => {
+  const defaultWords: Word[] = [
+    { english: 'apple', japanese: 'りんご' },
+    { english: 'banana', japanese: 'バナナ' },
+    { english: 'coffee', japanese: 'コーヒー' },
+    { english: 'tea', japanese: 'お茶' }
+  ]
+}
